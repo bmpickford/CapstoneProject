7536f8783ada888aaa091ff8661c1e683745afe3
Ben <b.mpickford@outlook.com>
2017-05-02 20:04:39 +1000

    initial commit

---
+++ app/src/main/java/com/app/capstone/qut_app/HomePage.java
@@ -0,0 +1,45 @@
+package com.app.capstone.qut_app;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomePage extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_gpa:
+                    mTextMessage.setText(R.string.title_gpa);
+                    return true;
+                case R.id.navigation_units:
+                    mTextMessage.setText(R.string.title_units);
+                    return true;
+                case R.id.navigation_badges:
+                    mTextMessage.setText(R.string.title_badges);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
