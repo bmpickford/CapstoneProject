96b1480ce28326017ebbe2fc59112b942982f735
Ben <b.mpickford@outlook.com>
2017-05-06 17:38:43 +1000

    Added button listener for goals

--- app/src/main/java/com/app/captsone/app/GoalsPage.java
+++ app/src/main/java/com/app/captsone/app/GoalsPage.java
@@ -3,10 +3,12 @@ package com.app.captsone.app;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -48,12 +50,19 @@ public class GoalsPage extends Fragment {
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+
+
+
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -64,7 +73,16 @@ public class GoalsPage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_goals_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_goals_page, container, false);
+        FloatingActionButton button = (FloatingActionButton) view.findViewById(R.id.addGoalButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button clicked");
+            }
+        });
+
+        return view;
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
