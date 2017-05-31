f9701a5dca194370beed5a8b1a52258813a6fabe
Ben <b.mpickford@outlook.com>
2017-05-31 17:10:42 +1000

    Added new content to course pages

--- app/src/main/java/com/app/capstone/app/NewGoalPage.java
+++ app/src/main/java/com/app/capstone/app/NewGoalPage.java
@@ -1,9 +1,21 @@
 package com.app.capstone.app;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public class NewGoalPage extends AppCompatActivity {
 
@@ -12,6 +24,50 @@ public class NewGoalPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_goal_page);
 
+        Button button = (Button) findViewById(R.id.goalSubmitButton);
+        final TextView edittext = (TextView) findViewById(R.id.dueDate);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        final Calendar myCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(edittext, myCalendar);
+            }
+
+            private void updateLabel(TextView edittext, Calendar myCalendar) {
+
+                String myFormat = "MM/dd/yy"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.ENGLISH);
+
+                edittext.setText(sdf.format(myCalendar.getTime()));
+
+            }
+
+        };
+
+        edittext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(NewGoalPage.this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
     }
-    
 }
